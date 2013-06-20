ca2c0b858969ed229f08fbcbd66fefa2c1b1a76e
Mat Roscoe <mat.roscoe@unb.ca>
2013-06-20 18:06:55 +0200

    Updated manifest started candidate extraction node.

---
+++ ros/include/ObjectCandidateExtraction.h
@@ -0,0 +1,18 @@
+/*
+ * ObjectCandidateExtraction.h
+ *
+ *  Created on: Jun 20, 2013
+ *      Author: badrobit
+ */
+
+#ifndef OBJECTCANDIDATEEXTRACTION_H_
+#define OBJECTCANDIDATEEXTRACTION_H_
+
+class ObjectCandidateExtraction
+{
+public:
+	ObjectCandidateExtraction();
+	virtual ~ObjectCandidateExtraction();
+};
+
+#endif /* OBJECTCANDIDATEEXTRACTION_H_ */
---
+++ ros/src/ObjectCandidateExtraction.cpp
@@ -0,0 +1,18 @@
+/*
+ * ObjectCandidateExtraction.cpp
+ *
+ *  Created on: Jun 20, 2013
+ *      Author: badrobit
+ */
+
+#include "ObjectCandidateExtraction.h"
+
+ObjectCandidateExtraction::ObjectCandidateExtraction() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ObjectCandidateExtraction::~ObjectCandidateExtraction() {
+	// TODO Auto-generated destructor stub
+}
+
